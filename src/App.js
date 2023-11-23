75ee6f986b01b0c2f1e8039dae9ea59986d3b88a
AniBukhrashvili <92999532+AniBukhrashvili@users.noreply.github.com>
2023-11-23 21:36:45 +0400

    markup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import AppAddButton from "./components/AppAddButton";
+import AppContainer from "./components/AppContainer";
+import AppHeader from "./components/AppHeader";
+import AppListing from "./components/AppListing";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer>
+      <div>
+        <AppHeader />
+        <AppListing />
+      </div>
+      <AppAddButton />
+    </AppContainer>
   );
 }
 
---
+++ src/components/AppModal/index.js
@@ -0,0 +1,5 @@
+import styles from "./AppModal.module.scss";
+
+export default function AppModal() {
+  return <div>AppModal</div>;
+}
